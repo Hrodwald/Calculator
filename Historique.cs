d17ecf4e9f2c29a891ba0afa12763187bdcec63e
PedroMartins2910 <martins.pedro2910@gmail.com>
2020-05-17 13:29:04 +0200

    fini
    quelques modifs graphiques a faire

---
+++ Historique.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Documents;
+
+namespace Calculator
+{
+    public class Historique : BaseNotifyPropertyChanged
+    {
+        public string SaisieHisto
+        {
+            get { return (string)GetField(); }
+            set { SetField(value); }
+        }
+        public string ResultatHisto
+        {
+            get { return (string)GetField(); }
+            set { SetField(value); }
+        }
+
+        }
+}
